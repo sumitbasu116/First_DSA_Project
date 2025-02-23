33a46285c54679fdbc94edc980cc022e060b8e44
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-02-23 17:44:16 +0530

    recursion

--- src/co/sumit/striversrecursion/CombinationSumUniqueRecursion.java
+++ src/co/sumit/striversrecursion/CombinationSumUniqueRecursion.java
@@ -22,37 +22,27 @@ public class CombinationSumUniqueRecursion {
 		f(arr,target,0,list);
 	}
 
-	private static void f(int[] arr, int target, int ind, List<Integer> list) {
-		if(target==0) {
-			list.stream().forEach(e->System.out.print(e+" "));
-			System.out.println();
-			return;
-		}
-		
-		/*
-		 * we need a for loop here, bcz, to form a unique combination, suppose we can start from index 1,
-		 * Then we can either choose the element from 2nd followed by 3rd followed 4th and so on, considering all are unique elements.
-		 * suppose we can start from index 2, Then we can either choose the element from 3rd followed 4th and so on.
-		 */
-		for(int i=ind;i<arr.length;i++) {
-			// Skip duplicates to ensure unique combinations.
-			/* Example 1:
-			 * {1,1,1,2,2} and target = 4, suppose, we choose {1,1,2}, here, the first two 1s are from the index 0th and 1st.
-			 * Hence, we can not choose 2nd index 1 as then it will create the same combination.
-			 * Example 2: {1,1,1,1,2,2} and target = 5, suppose, we choose {1,1,1,2}, here, the first three 1s are from the index 0th and 1st and 2nd.
-			 * Hence, we can not choose 1 from the 3rd index otherwise, it will create the same combination as {1,1,1,2}.
-			 */
-			if(i>ind && arr[i]==arr[i-1]) {
-				continue;
-			}
-			// If the current number is greater than the remaining target, break (as array is sorted).
-			if(arr[i]>target) {
-				break;
-			}
-			list.add(arr[i]);
-			f(arr,target-arr[i],i+1,list);
-			list.remove(list.size()-1);
-		}
+	private static void f(int[] arr, int target, int index, List<Integer> list) {
+		if (target == 0) {
+            System.out.println(list);
+            return;
+        }
+        
+        if (index >= arr.length || arr[index] > target) return;
+
+        // Pick the current element
+        list.add(arr[index]);
+        f(arr, target - arr[index], index + 1, list);
+        list.remove(list.size() - 1);
+
+        // Skip duplicates and move to the next distinct element
+        int nextIndex = index + 1;
+        while (nextIndex < arr.length && arr[nextIndex] == arr[index]) {
+            nextIndex++;
+        }
+        
+        // Do Not pick the element 
+        f(arr, target, nextIndex, list);
 		
 	}
 
